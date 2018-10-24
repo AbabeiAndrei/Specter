91815cce2bf51d782ebc7c829a7f79fcf379e7cd
Ababei Andrei <AbabeiAndrei@outlook.com>
2018-10-24 22:35:12 +0300

    Add config module

--- Specter.App/ClientApp/src/app/app.module.ts
+++ Specter.App/ClientApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { AppConfigModule } from '../services/app-config.module';
+//import { AppConfigModule } from '../services/app-config.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { AppConfigModule } from '../services/app-config.module';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    AppConfigModule,
+    //AppConfigModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- Specter.App/ClientApp/src/services/app-config.module.js
+++ Specter.App/ClientApp/src/services/app-config.module.js
@@ -1,23 +1,18 @@
 //import { NgModule, InjectionToken, Inject } from '@angular/core';
 //import { environment } from '../environments/environment';
-
 //export let APP_CONFIG = new InjectionToken<AppConfig>('config');
-
 //export class AppConfig {
 //  apiEndpoint: string;
 //}
-
 //export const APP_DI_CONFIG: AppConfig = {
 //  apiEndpoint: environment.baseEndpoint
 //};
-
 //@NgModule({
 //  providers: [{
 //    provide: APP_CONFIG,
 //    useValue: APP_DI_CONFIG
 //  }]
 //})
-  
 //export class AppConfigModule {
-
 //}
+//# sourceMappingURL=app-config.module.js.map
