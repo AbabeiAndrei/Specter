82ead7a8bb01e9dc2086173fd8d9a05b14000db8
Ababei Andrei <AbabeiAndrei@outlook.com>
2018-10-24 22:29:41 +0300

    Add project files.

---
+++ Specter.DataLayer/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Specter.DataLayer.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<Template> Templates { get; set; }
+        
+        public ICollection<Timesheet> Timesheets { get; set; }
+    }
+}
